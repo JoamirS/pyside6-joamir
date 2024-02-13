5a4a82b5ede7171a4696f238c43ba23161de0a78
Joamir Silveira <jmrsilveira@outlook.com>
2024-02-12 21:56:48 -0300

    Update class 01

--- PySide6/Class_01.py
+++ PySide6/Class_01.py
@@ -1,3 +1,4 @@
+
 """
 QApplication and QPushButton de PySide6.QtWidgets
 QApplication -> The main Widget of application
@@ -6,14 +7,23 @@ PySide6.QtWidgets -> Where is the PySide6 widgets
 """
 import sys
 
-from PySide6.QtWidgets import QApplication, QPushButton
+from PySide6.QtWidgets import QApplication, QPushButton, QVBoxLayout, QWidget
 
 app_one = QApplication(sys.argv)
 
 # Add the widget hierarchy and show window
 button_with_text = QPushButton('Texto do botão')
-button_with_text.setStyleSheet('font-size: 40px; color: blue;')
-button_with_text.show()
+button_with_text.setStyleSheet('font-size: 30px; color: blue;')
+
+
+central_widget = QWidget()
+# Create layout
+layout = QVBoxLayout()
+central_widget.setLayout(layout)
+
+layout.addWidget(button_with_text)
+
+central_widget.show()
 
 # Loop of application
 app_one.exec()
