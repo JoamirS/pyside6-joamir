af3d985de479e3cd365e5af9e88dc0db3e477276
Joamir Silveira <jmrsilveira@outlook.com>
2024-02-17 17:51:32 -0300

    update grid

--- PySide6/Class_01.py
+++ PySide6/Class_01.py
@@ -7,21 +7,28 @@ PySide6.QtWidgets -> Where is the PySide6 widgets
 """
 import sys
 
-from PySide6.QtWidgets import QApplication, QPushButton, QVBoxLayout, QWidget
+from PySide6.QtWidgets import QApplication, QPushButton, QVBoxLayout, QWidget, QGridLayout
 
 app_one = QApplication(sys.argv)
 
-# Add the widget hierarchy and show window
-button_with_text = QPushButton('Texto do botão')
-button_with_text.setStyleSheet('font-size: 30px; color: blue;')
+button_with_text_01 = QPushButton('Texto do botão 1')
+button_with_text_01.setStyleSheet('font-size: 30px; color: blue;')
+
+button_with_text_02 = QPushButton('Texto do botão 2')
+button_with_text_02.setStyleSheet('font-size: 30px; color: red;')
+
+button_with_text_03 = QPushButton('Texto do botão 3')
+button_with_text_03.setStyleSheet('font-size: 25px; color: green')
 
 
 central_widget = QWidget()
 # Create layout
-layout = QVBoxLayout()
+layout = QGridLayout()
 central_widget.setLayout(layout)
 
-layout.addWidget(button_with_text)
+layout.addWidget(button_with_text_01, 1, 1, 1, 1)
+layout.addWidget(button_with_text_02, 1, 2, 1, 1)
+layout.addWidget(button_with_text_03, 2, 1, 1, 2)
 
 central_widget.show()
 
--- PySide6/main.py
+++ PySide6/main.py
@@ -2,6 +2,5 @@
 
 import PySide6.QtCore
 
-
 print(PySide6.__version__)
 print(PySide6.QtCore.__version__)
